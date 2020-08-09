0a9a5727035869e533078a9c4cd006e2e1f857e6
Sekarri Solomon <ssolomonewsis@gmail.com>
2020-08-09 05:52:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array(array, element) << "arrays!"
+  add_element_to_end_of_array << "arrays!"
   puts add_element_to_end_of_array(array, element).inspect
   
 end
