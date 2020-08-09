c6201fd1acd1f0721752368e51e99521824cc1a5
Sekarri Solomon <ssolomonewsis@gmail.com>
2020-08-09 06:10:33 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["a","b","c","d"]
 end
 
 def add_element_to_end_of_array(array, element)
+  array.push("arrays!")
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  arrays = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  wow = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array [2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array [-1]
 end
